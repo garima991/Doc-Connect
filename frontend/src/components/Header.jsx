dd4d45055f807f6b8b30e18d25549848e215fe76
garima <saxenagarima2953@gmail.com>
2025-06-04 19:48:41 +0530

    Refactor components: update image sources, enhance Navbar links, and improve MyProfile layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                     Book Appointment <br />  With Trusted Doctors
                 </p>
                 <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
-                    <img className='w-28' src={assets.groupProfile} alt="" />
+                    <img className='w-28' src={assets.group_profiles} alt="" />
                     <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block' /> schedule your appointment hassle-free.</p>
                 </div>
                 <a href='#speciality' className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-[#595959] text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { assets } from "../assets/assets_frontend/assets";
 import { FaUserDoctor } from "react-icons/fa6";
 import { NavLink, useNavigate } from "react-router-dom";
@@ -8,66 +7,87 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]">
-      <a
-        href="#"
-        className="flex items-center"
-      >
-        <FaUserDoctor className="mr-2" />
-        DocConnect
+      <a href="#" className="flex items-center">
+        <FaUserDoctor className="mr-2 size-5" />
+        <span className="text-lg font-bold">DocConnect</span>
       </a>
 
       <ul className="md:flex items-start gap-5 font-medium hidden">
-        <NavLink to="/">
-          <li className="py-1">HOME</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "border-b-2 border-blue-400 pb-1"
+              : " hover:border-blue-200 pb-1"
+          }
+        >
+          <li>HOME</li>
         </NavLink>
-        <NavLink to="/doctors">
-          <li className="py-1">ALL DOCTORS</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+
+        <NavLink
+          to="/doctors"
+          className={({ isActive }) =>
+            isActive
+              ? "border-b-2 border-blue-400 pb-1"
+              : " hover:border-blue-200 pb-1"
+          }
+        >
+          <li>ALL DOCTORS</li>
         </NavLink>
-        <NavLink to="/about">
-          <li className="py-1">ABOUT</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive
+              ? "border-b-2 border-blue-400 pb-1"
+              : " hover:border-blue-200 pb-1"
+          }
+        >
+          <li>ABOUT</li>
         </NavLink>
-        <NavLink to="/contact">
-          <li className="py-1">CONTACT</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive
+              ? "border-b-2 border-blue-400 pb-1"
+              : " hover:border-blue-200 pb-1"
+          }
+        >
+          <li>CONTACT</li>
         </NavLink>
       </ul>
 
       <div className="flex items-center gap-4 ">
-        
-          <div className="flex items-center gap-2 cursor-pointer group relative">
-            <img className="w-8 rounded-full" src={''} alt="" />
-            <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
-              <div className="min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4">
-                <p
-                  onClick={() => navigate("/my-profile")}
-                  className="hover:text-black cursor-pointer"
-                >
-                  My Profile
-                </p>
-                <p
-                  onClick={() => navigate("/my-appointments")}
-                  className="hover:text-black cursor-pointer"
-                >
-                  My Appointments
-                </p>
-                <p className="hover:text-black cursor-pointer">
-                  Logout
-                </p>
-              </div>
+        <div className="flex items-center gap-2 cursor-pointer group relative">
+          <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
+          <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+          <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+            <div className="min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4">
+              <p
+                onClick={() => navigate("/my-profile")}
+                className="hover:text-black cursor-pointer"
+              >
+                My Profile
+              </p>
+              <p
+                onClick={() => navigate("/my-appointments")}
+                className="hover:text-black cursor-pointer"
+              >
+                My Appointments
+              </p>
+              <p className="hover:text-black cursor-pointer">Logout</p>
             </div>
           </div>
-       
-          {/* <button
-            onClick={() => navigate("/login")}
-            className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
-          >
-            Create account
-          </button> */}
-      
+        </div>
+
+        <button
+          onClick={() => navigate("/login")}
+          className="bg-blue-400 text-white px-8 py-3 rounded-full font-light hidden md:block"
+        >
+          Create account
+        </button>
+
         <img
           onClick={() => setShowMenu(true)}
           className="w-6 md:hidden"
